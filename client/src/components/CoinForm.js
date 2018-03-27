a2792bbfeecb62b724fca6146283fe128735b573
“TJ <tylerchecketts@gmail.com>
2018-03-26 19:31:45 -0600

    add actions, reducers, and start home components

---
+++ client/src/components/CoinForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CoinForm = () => (
+  <div>
+    Form
+  </div>
+)
+
+export default CoinForm;
---
+++ client/src/components/CoinList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CoinList = () => (
+  <div>
+    List
+  </div>
+)
+
+export default CoinList;
